0399eb2179401af35a5603117be9f303527d0e20
dolivadiya <doli@yudiz.com>
2017-12-08 14:15:00 +0530

    Initialize API add vender_uuid and Other new API

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -48,11 +48,18 @@ class AppController extends \lithium\action\Controller {
       'error'=>'OS Version missing!'
      )));
     }    
+    if($this->request->data['vendor_uuid']==null || $this->request->data['vendor_uuid']==""){
+     return $this->render(array('json' => array('success'=>0,
+      'now'=>time(),
+      'error'=>'Vendor uuid missing!'
+     )));
+    }    
     $key = md5(time());
     $data = array(
     'device_type'=>$this->request->data['device_type'],
     'os_version'=>$this->request->data['os_version'],
     'device_name'=>$this->request->data['device_name'],
+    'vendor_uuid'=>$this->request->data['vendor_uuid'],
     'DateTime' => new \MongoDate(),
     'IP' => $this->request->data['ip'],
     'key' => $key,
@@ -143,5 +150,300 @@ class AppController extends \lithium\action\Controller {
    }
  }
  
+ public function changePin($key = null)
+ {
+  if ($key==null || $key == ""){
+    return $this->render(array('json' => array('success'=>0,
+    'now'=>time(),
+    'error'=>'Key missing!'
+   )));
+  }
+  if($this->request->data){
+      if($this->request->data['pin']==null || $this->request->data['pin']==""){
+      return $this->render(array('json' => array('success'=>0,
+        'now'=>time(),
+        'error'=>'Pin missing!'
+      )));
+      }
+      if($this->request->data['new_pin']==null || $this->request->data['new_pin']==""){
+      return $this->render(array('json' => array('success'=>0,
+        'now'=>time(),
+        'error'=>'New Pin missing!'
+      )));
+      }
+
+      if(strlen($this->request->data['new_pin']) != 4){
+        return $this->render(array('json' => array('success'=>0,
+          'now'=>time(),
+          'error'=>'New pin must be 4 number!'
+        )));
+        }
+
+      if($this->request->data['pin'] == $this->request->data['new_pin']){
+        return $this->render(array('json' => array('success'=>0,
+          'now'=>time(),
+          'error'=>'New Pin same as current pin'
+        )));
+        }
+      
+        $record = Apps::find('first',array(
+          'conditions' => array(
+            'key'=>$key,
+            'pin'=>$this->request->data['pin'] )
+        )
+      );
+      if(count($record) > 0){
+        $data = array('pin' => $this->request->data['new_pin']);
+        $conditions =  array(
+          'key'=>$key,
+          'pin'=>$this->request->data['pin'] 
+        );
+        Apps::update($data, $conditions);
+        return $this->render(array('json' => array('success'=>1,
+        'now'=>time(),
+        'result'=>'New Pin updated',
+        'pin'=> $this->request->data['new_pin']
+      )));
+    }
+    else{ 
+      return $this->render(array('json' => array('success'=>0,
+        'now'=>time(),
+        'error'=>'Old Pin does not match',
+      )));
+    }
+    }
+  }
+
+  public function checkCurrencyUnit($key = null){
+
+    if ($key==null || $key == ""){
+      return $this->render(array('json' => array('success'=>0,
+      'now'=>time(),
+      'error'=>'Key missing!'
+      )));
+    }
+    $record = Apps::find('first',array(
+      'conditions' => array(
+        'key'=>$key
+        )
+      )
+    );
+    if(count($record) > 0){
+      if($record['unit'] == null){
+        $unit = 'One';
+        $data = array('unit' => 'One');
+        $conditions = array('key' => $key);
+        Apps::update($data, $conditions);
+      }else{
+        $unit = $record['unit'];        
+      }
+      return $this->render(array('json' => array('success'=>1,
+        'now'=>time(),
+        'result'=>'Current unit',
+        'pin'=> $unit
+      )));
+    }else{
+      return $this->render(array('json' => array('success'=>0,
+        'now'=>time(),
+        'error'=>'Key does not match',
+      )));
+    }
+  }
+  public function updateCurrencyUnit($key = null)
+  {
+    if ($key==null || $key == ""){
+      return $this->render(array('json' => array('success'=>0,
+      'now'=>time(),
+      'error'=>'Key missing!'
+      )));
+    }
+
+    if ($this->request->data['unit']==null || $this->request->data['unit'] == ""){
+      return $this->render(array('json' => array('success'=>0,
+      'now'=>time(),
+      'error'=>'unit missing!'
+      )));
+    }
+
+
+    $record = Apps::find('first',array(
+      'conditions' => array(
+        'key'=>$key
+        )
+      )
+    );
+    if(count($record) > 0){
+        $unit = $this->request->data['unit'];
+        $data = array('unit' => $unit);
+        $conditions = array('key' => $key);
+        Apps::update($data, $conditions);
+      return $this->render(array('json' => array('success'=>1,
+        'now'=>time(),
+        'result'=>'Update unit',
+        'pin'=> $unit
+      )));
+    }else{
+      return $this->render(array('json' => array('success'=>0,
+        'now'=>time(),
+        'error'=>'Key does not match',
+      )));
+    }
+  }
+  
+  public function checkEmailNotification($key = null)
+  {
+    if ($key==null || $key == ""){
+      return $this->render(array('json' => array('success'=>0,
+      'now'=>time(),
+      'error'=>'Key missing!'
+      )));
+    }
+    $record = Apps::find('first',array(
+      'conditions' => array(
+        'key'=>$key
+        )
+      )
+    );
+    if(count($record) > 0){
+      if($record['email_notification'] == null){
+        $email_notification = 0;
+        $data = array('email_notification' => 0);
+        $conditions = array('key' => $key);
+        Apps::update($data, $conditions);
+      }else{
+        $email_notification = $record['email_notification'];        
+      }
+      return $this->render(array('json' => array('success'=>1,
+        'now'=>time(),
+        'result'=>'Email Notification',
+        'email_notification'=> $email_notification
+      )));
+    }else{
+      return $this->render(array('json' => array('success'=>0,
+        'now'=>time(),
+        'error'=>'Key does not match',
+      )));
+    }         
+  }
+    
+  public function updateEmailNotification($key = null)
+  {
+    if ($key==null || $key == ""){
+      return $this->render(array('json' => array('success'=>0,
+      'now'=>time(),
+      'error'=>'Key missing!'
+      )));
+    }
+
+    if ($this->request->data['email_notification']==null || $this->request->data['email_notification'] == ""){
+      return $this->render(array('json' => array('success'=>0,
+      'now'=>time(),
+      'error'=>'email notification status missing!'
+      )));
+    }
+
+    $record = Apps::find('first',array(
+      'conditions' => array(
+        'key'=>$key
+        )
+      )
+    );
+    if(count($record) > 0){
+        $email_notification = $this->request->data['email_notification'];
+        $data = array('email_notification' => $email_notification);
+        $conditions = array('key' => $key);
+        Apps::update($data, $conditions);
+      
+      return $this->render(array('json' => array('success'=>1,
+        'now'=>time(),
+        'result'=>'Email notification Updated',
+        'email_notification'=> $email_notification
+      )));
+    }else{
+      return $this->render(array('json' => array('success'=>0,
+        'now'=>time(),
+        'error'=>'Key does not match',
+      )));
+    }
+  }
+  
+  public function checkSmsNotification($key = null)
+  {
+    if ($key==null || $key == ""){
+      return $this->render(array('json' => array('success'=>0,
+      'now'=>time(),
+      'error'=>'Key missing!'
+      )));
+    }
+    $record = Apps::find('first',array(
+      'conditions' => array(
+        'key'=>$key
+        )
+      )
+    );
+    if(count($record) > 0){
+      if($record['sms_notification'] == null){
+        $sms_notification = 0;
+        $data = array('sms_notification' => 0);
+        $conditions = array('key' => $key);
+        Apps::update($data, $conditions);
+      }else{
+        $sms_notification = $record['sms_notification'];        
+      }
+      return $this->render(array('json' => array('success'=>1,
+        'now'=>time(),
+        'result'=>'Sms notification',
+        'sms_notification'=> $sms_notification
+      )));
+    }else{
+      return $this->render(array('json' => array('success'=>0,
+        'now'=>time(),
+        'error'=>'Key does not match',
+      )));
+    }  
+  }
+    
+  public function updateSmsNotification($key = null)
+  {
+    if ($key==null || $key == ""){
+      return $this->render(array('json' => array('success'=>0,
+      'now'=>time(),
+      'error'=>'Key missing!'
+      )));
+    }
+
+    if ($this->request->data['sms_notification']==null || $this->request->data['sms_notification'] == ""){
+      return $this->render(array('json' => array('success'=>0,
+      'now'=>time(),
+      'error'=>'sms notification status missing!'
+      )));
+    }
+
+    $record = Apps::find('first',array(
+      'conditions' => array(
+        'key'=>$key
+        )
+      )
+    );
+    if(count($record) > 0){
+        $sms_notification = $this->request->data['sms_notification'];
+        $data = array('sms_notification' => $sms_notification);
+        $conditions = array('key' => $key);
+        Apps::update($data, $conditions);
+      
+      return $this->render(array('json' => array('success'=>1,
+        'now'=>time(),
+        'result'=>'Sms notification Updated',
+        'sms_notification'=> $sms_notification
+      )));
+    }else{
+      return $this->render(array('json' => array('success'=>0,
+        'now'=>time(),
+        'error'=>'Key does not match',
+      )));
+    }
+  }
+
+
 }
 ?>
