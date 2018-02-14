d0fe251bf6babc3e9453bcf78cea874e28b4b6d7
Nilam Doctor <nilamdoc@gmail.com>
2018-02-14 11:46:51 +0530

    remove translation

--- app/views/ex/setup.mail.php
+++ app/views/ex/setup.mail.php
@@ -1,35 +1,35 @@
 <?php
-use lithium\g11n\Message;
-extract(Message::aliases());
+//use lithium\g11n\Message;
+//extract(Message::aliases());
 ?>
-<h3><?=$t('Welcome')?> <?=$compact['data']['email']?>,</h3>
-<h4><?=$t('This email communication is CONFIDENTIAL. All the information given in the document is financial sensitive.')?></h4>
+<h3>Welcome <?=$compact['data']['email']?>,</h3>
+<h4>This email communication is CONFIDENTIAL. All the information given in the document is financial sensitive.</h4>
 
 
-<p><?=$t('You have today created your own XGC wallet. This is the second confidential email you are receiving regarding your new account.')?></p>
+<p>You have today created your own XGC wallet. This is the second confidential email you are receiving regarding your new account.</p>
 
-<p><?=$t('Enclosed is a PDF document which contains a password recovery, public and private keys of XGC in PDF document.')?></p>
+<p>Enclosed is a PDF document which contains a password recovery, public and private keys of XGC in PDF document.</p>
 
-<h4><?=$t('Print the PDF document and keep it in a safe place.')?></h4>
-<h4><?=$t('You will need this if at any time you lose your password or keys.')?>
+<h4>Print the PDF document and keep it in a safe place.</h4>
+<h4>You will need this if at any time you lose your password or keys.
 
-<?=$t('For extra security we recommend that you delete  this email after you have stored your PDF in a safe location.')?> </h4>
-<p><?=$t('We do not save password on the XGCWallet site. So the only way to recover the account information is through the passphrase provided in the PDF document')?></p>
-
-
-<p><?=$t('GreenCoinX (XGC) is an identified crypto currency.')?> <?=$t('You can receive coins directly to email')?> <strong><?=$compact['data']['email']?></strong> <?=$t('or phone')?> <strong>+<?=$compact['data']['phone']?></strong>. <?=$t('Please avoid using the public key as used in other crypto currencies. You can also send coins directly to any other GreenCoinX user by using their registered  email address or mobile phone number.')?>
+For extra security we recommend that you delete  this email after you have stored your PDF in a safe location. </h4>
+<p>We do not save password on the XGCWallet site. So the only way to recover the account information is through the passphrase provided in the PDF document</p>
 
 
+<p>GreenCoinX (XGC) is an identified crypto currency. You can receive coins directly to email <strong><?=$compact['data']['email']?></strong> or phone <strong>+<?=$compact['data']['phone']?></strong>. Please avoid using the public key as used in other crypto currencies. You can also send coins directly to any other GreenCoinX user by using their registered  email address or mobile phone number.
 
 
-<p><?=$t('Thank you for choosing XGCWallet')?>,</p>
+
+
+<p>Thank you for choosing XGCWallet,</p>
 <h5><a href="https://XGCWallet.org">XGCWallet</a></h5>
 
-<p><small><?=$t('This email communication is CONFIDENTIAL. All the information given in the document is financial sensitive. If you are not the intended recipient, please notify the sender by email to support@xgcwallet.org and delete this communication and any attachments and copies associated therewith from your computer immediately. Any dissemination or use of this information by a person other than the intended recipient is unauthorized and may be illegal. Thank you for your assistance and co-operation.')?>
+<p><small>This email communication is CONFIDENTIAL. All the information given in the document is financial sensitive. If you are not the intended recipient, please notify the sender by email to support@xgcwallet.org and delete this communication and any attachments and copies associated therewith from your computer immediately. Any dissemination or use of this information by a person other than the intended recipient is unauthorized and may be illegal. Thank you for your assistance and co-operation.
 </small></p>
 <p>
 	<small>
-	<?=$t('IP address is recorded on the server')?><br>
-	<?=$t('Date')?>: <?=gmdate('Y M d H:m:s',$compact['data']['DateTime']->sec);?>
+	IP address is recorded on the server<br>
+	Date: <?=gmdate('Y M d H:m:s',$compact['data']['DateTime']->sec);?>
 	</small>
 </p>
--- app/views/ex/setupwd.mail.php
+++ app/views/ex/setupwd.mail.php
@@ -2,25 +2,25 @@
 use lithium\g11n\Message;
 extract(Message::aliases());
 ?>
-<h3><?=$t('Welcome')?> <?=$compact['data']['email']?>,</h3>
-<p><?=$t('Thank you for creating your GreenCoinX online wallet. This is the first of two confidential emails you will be receiving regarding your new account.')?></p>
+<h3>Welcome <?=$compact['data']['email']?>,</h3>
+<p>Thank you for creating your GreenCoinX online wallet. This is the first of two confidential emails you will be receiving regarding your new account.</p>
 
-<p><?=$t('You can access your account by using your email and passphrase.')?> <?=$t('Click the link')?> <a href="https://xgcwallet.org/wallet/login/<?=$compact['data']['walletid']?>">https://xgcwallet.org/wallet/login/<?=$compact['data']['walletid']?></a> <?=$t('to sign in to your wallet')?>.</p> 
-<h4><?=$t('SECOND ACCOUNT OPENING EMAIL FOLLOWS SEPARATELY')?><h4>
-<p><?=$t('We will also be sending you second email with a PDF attachment which contains a password recovery, public and private keys of XGC. The PDF document is password protected.')?> <strong><?=$t('The PDF document password is your email address')?></strong>. <?=$t('The password is case sensitive.')?></p>
-
-
-<p><?=$t('GreenCoinX (XGC) is an identified crypto currency.')?> <?=$t('You can receive coins directly to email')?> <strong><?=$compact['data']['email']?></strong> <?=$t('or phone')?> <strong>+<?=$compact['data']['phone']?></strong>. <?=$t('Please avoid using the public key as used in other crypto currencies. You can also send coins directly to anyone registered on XGCWallet or using GreenCoinX (XGC).')?>
+<p>You can access your account by using your email and passphrase. Click the link <a href="https://xgcwallet.org/wallet/login/<?=$compact['data']['walletid']?>">https://xgcwallet.org/wallet/login/<?=$compact['data']['walletid']?></a> to sign in to your wallet.</p> 
+<h4>SECOND ACCOUNT OPENING EMAIL FOLLOWS SEPARATELY<h4>
+<p>We will also be sending you second email with a PDF attachment which contains a password recovery, public and private keys of XGC. The PDF document is password protected. <strong>The PDF document password is your email address</strong>. The password is case sensitive.</p>
 
 
-<p><?=$t('We respect your privacy and security.')?></p>
+<p>GreenCoinX (XGC) is an identified crypto currency. You can receive coins directly to email <strong><?=$compact['data']['email']?></strong> or phone <strong>+<?=$compact['data']['phone']?></strong>. Please avoid using the public key as used in other crypto currencies. You can also send coins directly to anyone registered on XGCWallet or using GreenCoinX (XGC).
 
-<p><?=$t('Thank you for choosing XGCWallet')?>,</p>
+
+<p>We respect your privacy and security.</p>
+
+<p>Thank you for choosing XGCWallet,</p>
 <h5><a href="https://XGCWallet.org">XGCWallet</a></h5>
-<p><small><?=$t('This email communication is CONFIDENTIAL. All the information given in the document is financial sensitive. If you are not the intended recipient, please notify the sender by email to support@xgcwallet.org and delete this communication and any attachments and copies associated therewith from your computer immediately. Any dissemination or use of this information by a person other than the intended recipient is unauthorized and may be illegal. Thank you for your assistance and co-operation.')?></small></p>
+<p><small>This email communication is CONFIDENTIAL. All the information given in the document is financial sensitive. If you are not the intended recipient, please notify the sender by email to support@xgcwallet.org and delete this communication and any attachments and copies associated therewith from your computer immediately. Any dissemination or use of this information by a person other than the intended recipient is unauthorized and may be illegal. Thank you for your assistance and co-operation.</small></p>
 <p>
 	<small>
-	<?=$t('IP address is recorded on the server')?><br>
-	<?=$t('Date')?>: <?=gmdate('Y M d H:m:s',$compact['data']['DateTime']->sec);?>
+	IP address is recorded on the server<br>
+	Date: <?=gmdate('Y M d H:m:s',$compact['data']['DateTime']->sec);?>
 	</small>
 </p>
